50c1a81e454a35ce61e1dc9569613e22c39affb9
filiporastovic@gmail.com <filiporastovic@gmail.com>
2018-11-12 18:46:30 +0100

    pff

--- wp-content/themes/stargazer-workshop/header.php
+++ wp-content/themes/stargazer-workshop/header.php
@@ -30,10 +30,22 @@
 </head>
 
 <body>
-	<?php
-		// wp_nav_menu( array(
-		// 	'theme_location' => 'primary',
-		// 	'container'      => false,
-		// 	'menu_class'     => 'nav navbar-nav navbar-right'
-		// ));
-	?>
+	<!-- Navigation -->
+	<!-- <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+			<?php
+				// wp_nav_menu( array(
+				// 	'theme_location' => 'primary',
+				// 	'container'      => false,
+				// 	'menu_class'     => 'navbar-nav ml-auto'
+				// ));
+			?>	
+        </div>
+      </div>
+    </nav> -->
--- wp-content/themes/stargazer-workshop/page-homepage.php
+++ wp-content/themes/stargazer-workshop/page-homepage.php
@@ -9,9 +9,91 @@ get_header();
 ?>
 
 
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias laboriosam porro beatae dolorem repellat, optio assumenda at cupiditate reiciendis quia, commodi quibusdam ratione illo, quam vitae est ipsam perspiciatis architecto!
-<h2>Recent Posts</h2>
-<ul>
+
+        <header class="masthead">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-6 col-md-3 ">
+                <div class="scifi-panel text-center">
+                  <a class="btn-scifi" href="#">Stargazer Workshop</a>
+                </div>
+              </div>
+              <div class="col-6 col-md-5">
+                  <!-- <div class="scene">
+                    <div class="wrapper">
+                      <div class="globe">
+                        <span class="ring"></span>
+                        <span class="ring"></span>
+                        <span class="ring"></span>
+                        <span class="ring"></span>
+                        <span class="ring"></span>
+                        <span class="ring"></span>
+                        <span class="ring"></span>
+                        <span class="ring"></span>
+                        <span class="ring"></span>
+                        <span class="ring"></span>
+                      </div>
+                    </div>
+                  </div> -->
+                  <!-- <canvas id="displayCanvas" width="300px" height="220px">
+                      Your browser does not support HTML5 canvas.
+                  </canvas> -->
+                  <div class="scifi-panel">
+                    <nav class="navbar navbar-expand-lg navbar-light " id="mainNav">
+                      <div class="container">
+                        <button class="navbar-toggler btn-scifi text-center" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                          Open <br>Menu
+                          
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarResponsive">
+                          <ul class="navbar-nav mx-auto">
+                            <li class="nav-item">
+                              <a class="nav-link" href="index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="about.html">About</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="post.html">Sample Post</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="contact.html">Contact</a>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </nav>
+                  </div>
+                </div>
+            </div>
+            <div class="row">
+              <div class="col-12 col-md-8">
+                <div class="scifi-panel scifi-panel-header text-center">
+                  <h1>Stargazer Workshop</h1>
+                  <p class="header-paragraph">
+                    Open Source Bootstrap Website Themes
+                  </p>
+                  
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-6 col-md-3">
+                <div class="scifi-panel text-center">
+                  <a class="btn-scifi" href="#">Find Out How</a>
+                </div>
+              </div>
+              <div class="col-6 col-md-5">
+                <div class="scifi-panel text-center">
+                  <a href="#" class="btn-scifi">Request a quote</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+
+
+
 <?php
     $args = array( 'numberposts' => '5' );
     $recent_posts = wp_get_recent_posts( $args );
