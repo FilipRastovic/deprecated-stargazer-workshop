e362a06c4e24f8562f14c7c260285386810083bf
filiporastovic@gmail.com <filiporastovic@gmail.com>
2018-11-16 17:36:12 +0100

    -color tweaks

--- gulp/build/js/stargazer.js
+++ gulp/build/js/stargazer.js
@@ -40,3 +40,38 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+var title = document.querySelector("p");
+var CHAR_TIME = 30;
+
+var text = void 0,index = void 0;
+
+function requestCharAnimation(char, value) {
+  setTimeout(function () {
+    char.textContent = value;
+    char.classList.add("fade-in");
+  }, CHAR_TIME);
+}
+
+function addChar() {
+  var char = document.createElement("span");
+  char.classList.add("char");
+  char.textContent = "▌";
+  title.appendChild(char);
+  requestCharAnimation(char, text.substr(index++, 1));
+  if (index < text.length) {
+    requestChar();
+  }
+}
+
+function requestChar() {var delay = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+  setTimeout(addChar, CHAR_TIME + delay);
+}
+
+function start() {
+  index = 0;
+  text = title.textContent.trim();
+  title.textContent = "";
+  requestChar(1000);
+}
+start();
